2cbaaebf97738820baf2f64324a5cb88ed70246a
Joven Lusterio <jovenlusterio16@gmail.com>
2026-07-31 23:12:33 +0800

    Initial Commit

---
+++ ConsTracTests/ConsTracTests.swift
@@ -0,0 +1,18 @@
+//
+//  ConsTracTests.swift
+//  ConsTracTests
+//
+//  Created by maria gemma lusterio on 7/31/26.
+//
+
+import Testing
+
+struct ConsTracTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
